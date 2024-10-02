610a3ee31e702c67902da34dee1e4664f4668ef2
Andrea Patricia <andreapatriciazr@gmail.com>
2024-10-02 16:28:06 -0700

    Modificaciones ejercicio

--- Codigo5.java
+++ Codigo5.java
@@ -1,29 +1,36 @@
+import java.util.Scanner;
+
 public class Codigo5 {
 
-	    Scanner s = new Scanner();
-	    System.out.print("Introduzca un número: ');
-	    String ni = s.nextLine();
-	    int c = ni;
-	    
-	    int afo = 0;
-	    int noAfo = 0;
-	    
-	    while (ni > 0) {
-		  int digito = (int)(ni % 10);
-	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
-			afo++;
-	      } else {
-			noAfo++;
-		  
-		  ni /= 10;
-	    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in); // crear scanner 
+        System.out.print("Introduzca un número: "); // introducir numero
+        String ni = s.nextLine(); // leer string ni 
+        int c = Integer.parseInt(ni); // convertir input string a integer
+        
+        int afo = 0;
+        int noAfo = 0;
+        
+        int originalNumber = c;
 
-	    if (afo > noAfo) {
-	      System.out.prinln("El " + c + " es un número afortunado.");
-	    } else {
-	      System.out.println("El " + c + " no es un número afortunado.");
-	    }
-	    
-	  }
-	  
-	}
+        while (c > 0) { // corregir c
+            int digito = c % 10;  
+            
+            if (digito == 3 || digito == 7 || digito == 8 || digito == 9) {
+                afo++;
+            } else {
+                noAfo++;
+            } // if / else
+            
+            c /= 10;  
+        } // while
+
+        if (afo > noAfo) {
+            System.out.println("El " + originalNumber + " es un número afortunado."); // corregir ortografía en el sysout
+        } else {
+            System.out.println("El " + originalNumber + " no es un número afortunado.");
+        } // if / else
+        
+        s.close(); // cerrar scanner
+    } // main 
+} // class 
